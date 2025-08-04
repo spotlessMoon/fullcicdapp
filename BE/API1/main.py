82d54762a0e0bc027bfd3b6b061612ffe07da42d
KB <k.b.bastawadi@gmail.com>
2025-08-04 13:51:43 +0530

    Initial commit of fullcicd project

---
+++ BE/API1/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Use specific domains in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/api/message")
+def get_message():
+    return {"service": "FastAPI", "message": "Hello from FastAPI!"}
---
+++ BE/API2/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.get('/api/message', (req, res) => {
+  res.json({ service: "Node.js", message: "Hello from Node.js!" });
+});
+
+app.listen(8002, () => {
+  console.log("Node service running on port 8002");
+});
+
